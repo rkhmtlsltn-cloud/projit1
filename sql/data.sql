c863ba1eaa26386a2042d072e584c118239b24f2
rkhmtlsltn-cloud <rkhmtl.sltn@gmail.com>
2026-01-19 16:13:20 +0500

    Create data.sql

---
+++ sql/data.sql
@@ -0,0 +1,8 @@
+insert into comics(title, price, stock) values
+('Goodnight Punpun Vol. 1', 6000, 4),
+('Chainsaw Man Vol. 1', 5500, 6),
+('Berserk Vol. 1', 6200, 3);
+
+insert into customers(name) values
+('Aruzhan'),
+('Dias');
