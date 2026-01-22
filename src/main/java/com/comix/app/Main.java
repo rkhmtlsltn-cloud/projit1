db44e8ef94e1c990d7c2648b85f40abfbe4129e1
rkhmtlsltn-cloud <rkhmtl.sltn@gmail.com>
2026-01-22 13:37:43 +0500

    Update Main.java

--- src/main/java/com/comix/app/Main.java
+++ src/main/java/com/comix/app/Main.java
@@ -14,46 +14,50 @@ public class Main {
 
         while (true) {
             System.out.println("\n1 Show all\n2 View by id\n3 Add manga\n4 Buy manga\n0 Exit");
-            String c = sc.nextLine();
 
             try {
-                if (c.equals("1")) {
-                    for (Comic x : comicDao.getAll())
-                        System.out.println(x.id + " " + x.title + " " + x.price + " stock=" + x.stock);
-
-                } else if (c.equals("2")) {
-                    System.out.print("Id: ");
-                    Comic x = comicDao.getById(Integer.parseInt(sc.nextLine()));
-                    System.out.println(x == null ? "Not found" :
-                            x.id + "\n" + x.title + "\n" + x.price + "\n" + x.stock);
-
-                } else if (c.equals("3")) {
-                    System.out.print("Title: ");
-                    String t = sc.nextLine();
-                    System.out.print("Price: ");
-                    double p = Double.parseDouble(sc.nextLine());
-                    System.out.print("Stock: ");
-                    int s = Integer.parseInt(sc.nextLine());
-                    comicDao.add(t, p, s);
-                    System.out.println("Added");
-
-                } else if (c.equals("4")) {
-                    System.out.print("Customer id: ");
-                    int cid = Integer.parseInt(sc.nextLine());
-                    System.out.print("Comic id: ");
-                    int mid = Integer.parseInt(sc.nextLine());
-                    System.out.print("Qty: ");
-                    int q = Integer.parseInt(sc.nextLine());
-                    int r = orderDao.buy(cid, mid, q);
-                    System.out.println(r < 0 ? "Buy error" : "Order id = " + r);
-
-                } else if (c.equals("0")) break;
-                else System.out.println("Wrong choice");
+                switch (sc.nextLine()) {
+                    case "1" -> {
+                        for (Comic x : comicDao.getAll())
+                            System.out.println(x.id + " " + x.title + " " + x.price + " stock=" + x.stock);
+                    }
+                    case "2" -> {
+                        System.out.print("Id: ");
+                        Comic x = comicDao.getById(Integer.parseInt(sc.nextLine()));
+                        System.out.println(x == null ? "Not found" :
+                                x.id + "\n" + x.title + "\n" + x.price + "\n" + x.stock);
+                    }
+                    case "3" -> {
+                        System.out.print("Title: ");
+                        String t = sc.nextLine();
+                        System.out.print("Price: ");
+                        double p = Double.parseDouble(sc.nextLine());
+                        System.out.print("Stock: ");
+                        int s = Integer.parseInt(sc.nextLine());
+                        comicDao.add(t, p, s);
+                        System.out.println("Added");
+                    }
+                    case "4" -> {
+                        System.out.print("Customer id: ");
+                        int cid = Integer.parseInt(sc.nextLine());
+                        System.out.print("Comic id: ");
+                        int mid = Integer.parseInt(sc.nextLine());
+                        System.out.print("Qty: ");
+                        int q = Integer.parseInt(sc.nextLine());
+                        int r = orderDao.buy(cid, mid, q);
+                        System.out.println(r < 0 ? "Buy error" : "Order id = " + r);
+                    }
+                    case "0" -> {
+                        sc.close();
+                        return;
+                    }
+                    default -> System.out.println("Wrong choice");
+                }
             } catch (Exception e) {
                 System.out.println("Error");
             }
         }
-        sc.close();
     }
 }
+
 
