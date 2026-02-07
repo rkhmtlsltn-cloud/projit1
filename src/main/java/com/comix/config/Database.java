d483f2224e099eb331926e9414b6a7af7c1fae6e
rkhmtlsltn-cloud <rkhmtl.sltn@gmail.com>
2026-02-07 23:07:26 +0500

    Update Database.java

--- src/main/java/com/comix/config/Database.java
+++ src/main/java/com/comix/config/Database.java
@@ -1,27 +1,42 @@
 package com.comix.config;
 
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
 
 public class Database {
 
     private static Database instance;
 
+    private final String url;
+    private final String user;
+    private final String pass;
+
     private Database() {
+        Properties p = new Properties();
+
+        try {
+            InputStream in = Database.class.getClassLoader().getResourceAsStream("db.properties");
+            if (in != null) {
+                p.load(in);
+                in.close();
+            }
+        } catch (Exception e) {
+        }
+
+        this.url = p.getProperty("db.url", "jdbc:postgresql://localhost:5432/comix_db");
+        this.user = p.getProperty("db.user", "postgres");
+        this.pass = p.getProperty("db.pass", "0000");
     }
 
     public static Database getInstance() {
-        if (instance == null) {
-            instance = new Database();
-        }
+        if (instance == null) instance = new Database();
         return instance;
     }
 
     public Connection getConnection() throws Exception {
-        return DriverManager.getConnection(
-                "jdbc:postgresql://localhost:5432/comix_db",
-                "postgres",
-                "0000"
-        );
+        return DriverManager.getConnection(url, user, pass);
     }
 }
+
