4098724a0c2bbe35461f467bbea064ed441faaa2
rkhmtlsltn-cloud <rkhmtl.sltn@gmail.com>
2026-02-07 23:11:11 +0500

    Update CustomerDao.java

--- src/main/java/com/comix/dao/CustomerDao.java
+++ src/main/java/com/comix/dao/CustomerDao.java
@@ -10,25 +10,27 @@ import java.sql.ResultSet;
 public class CustomerDao {
 
     public Customer getById(int id) throws Exception {
-        Connection con = Database.getInstance().getConnection();
-
-        PreparedStatement st = con.prepareStatement(
-                "select id, name, role from customers where id=?"
-        );
-        st.setInt(1, id);
-
-        ResultSet rs = st.executeQuery();
-
         Customer c = null;
-        if (rs.next()) {
-            c = new Customer(
-                    rs.getInt(1),
-                    rs.getString(2),
-                    rs.getString(3)
-            );
+
+        try (Connection con = Database.getInstance().getConnection();
+             PreparedStatement st = con.prepareStatement(
+                     "select id, name, role from customers where id=?"
+             )) {
+
+            st.setInt(1, id);
+
+            try (ResultSet rs = st.executeQuery()) {
+                if (rs.next()) {
+                    c = new Customer(
+                            rs.getInt(1),
+                            rs.getString(2),
+                            rs.getString(3)
+                    );
+                }
+            }
         }
 
-        con.close();
         return c;
     }
 }
+
