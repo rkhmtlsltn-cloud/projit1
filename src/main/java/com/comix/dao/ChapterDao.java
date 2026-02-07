dbdd274ebc402a174ac2b88e523abcc3e8813c84
rkhmtlsltn-cloud <rkhmtl.sltn@gmail.com>
2026-02-07 23:10:54 +0500

    Update ChapterDao.java

--- src/main/java/com/comix/dao/ChapterDao.java
+++ src/main/java/com/comix/dao/ChapterDao.java
@@ -13,22 +13,24 @@ public class ChapterDao {
     public ArrayList<Chapter> getByComic(int comicId) throws Exception {
         ArrayList<Chapter> list = new ArrayList<>();
 
-        Connection con = Database.getInstance().getConnection();
-        PreparedStatement st = con.prepareStatement(
-                "select chapter_number, title, text from comic_chapters where comic_id=? order by chapter_number"
-        );
-        st.setInt(1, comicId);
+        try (Connection con = Database.getInstance().getConnection();
+             PreparedStatement st = con.prepareStatement(
+                     "select chapter_number, title, text from comic_chapters where comic_id=? order by chapter_number"
+             )) {
 
-        ResultSet rs = st.executeQuery();
-        while (rs.next()) {
-            list.add(new Chapter(
-                    rs.getInt(1),
-                    rs.getString(2),
-                    rs.getString(3)
-            ));
+            st.setInt(1, comicId);
+
+            try (ResultSet rs = st.executeQuery()) {
+                while (rs.next()) {
+                    list.add(new Chapter(
+                            rs.getInt(1),
+                            rs.getString(2),
+                            rs.getString(3)
+                    ));
+                }
+            }
         }
 
-        con.close();
         return list;
     }
 }
